1acaa7a4a0138fd28ceb291db4e1ce0cf2829478
Matheus Carvalho <matheus.dev23@gmail.com>
2023-01-02 15:44:42 -0300

    Configuração de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Routes";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hellow World</h1>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/pages/CompleteOrder/index.tsx
@@ -0,0 +1,7 @@
+export function CompleteOrder() {
+  return (
+    <div>
+      <h1>Complete</h1>
+    </div>
+  );
+}

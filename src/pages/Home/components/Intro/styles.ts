b64a2a0f770f4a385afa94ae223233afb69e28c1
Matheus Carvalho <matheus.dev23@gmail.com>
2023-01-02 18:27:18 -0300

    Intro Home

---
+++ src/pages/Home/components/Intro/styles.ts
@@ -0,0 +1,34 @@
+import { rgba } from "polished";
+import styled from "styled-components";
+import introBackgroundImg from "../../../../assets/intro-background.svg";
+import { TitleText } from "../../../../components/Typografy";
+
+export const IntroContainer = styled.section`
+  width: 100%;
+  height: 34rem;
+
+  background: ${({ theme }) => `url(${introBackgroundImg}) no-repeat center,
+      linear-gradient(
+        0deg,
+        ${theme.colors["base-white"]} 0%,
+        ${rgba(theme.colors["base-background"], 0.2)} 50%,
+        ${theme.colors["base-background"]} 100%
+      )`};
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
+`;
+
+export const IntroContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.5rem;
+`;
+
+export const IntroTitle = styled(TitleText)`
+  margin-bottom: 1rem;
+`;
